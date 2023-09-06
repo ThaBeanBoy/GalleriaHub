5601e87fd16e436e2da4b1caf64bb15792f810f6
ThaBeanBoy <gtchipoyera@gmail.com>
2023-09-06 11:44:55 +0200

    Added colors to the tailwind config & applied them as the default text color & the backgeound-color of the pages

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        white: "#FFFAF5",
+        black: {
+          DEFAULT: "#475569",
+          x2: "#27313F",
+        },
+
+        grey: {
+          DEFAULT: "#B1B1B1",
+          light: "#E2E8F0",
+          control: "#DEE3E9",
+        },
+
+        active: {
+          DEFAULT: "#3667DC",
+          light: "#7D9CE8",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
