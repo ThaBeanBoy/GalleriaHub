db7521682ac954cd01e25e47408b4c103221fbb2
ThaBeanBoy <gtchipoyera@gmail.com>
2023-09-17 15:03:39 +0200

    got my old css file that defined the base styles of the web site

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col">
+    <main className="prose flex flex-col">
       <h1 className="mb-8">Hello World</h1>
     </main>
   );
