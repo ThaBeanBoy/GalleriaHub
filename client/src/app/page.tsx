75becb08a9363f602324813fc7227608bf41f173
ThaBeanBoy <gtchipoyera@gmail.com>
2023-09-17 14:59:34 +0200

    merged shadcn's tailwind config with the generated nextjs's generated tailwind config

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,70 +1,7 @@
-import ComponentDocumentation from "@/components/component-document";
-
-import { ComponentDocumentationProps } from "@/components/component-document";
-
-const documentedComponents: ComponentDocumentationProps[] = [
-  {
-    name: "Header 1",
-    code: `<h1>Home Page</h1>`,
-    render: <h1>Home Page</h1>,
-    description: "Should be used sparingly",
-  },
-  {
-    name: "Header 2",
-    code: "<h2>h2</h2>",
-    render: <h2>h2</h2>,
-  },
-];
-
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <h1 className="mb-8">Typography</h1>
-
-      {documentedComponents.map((props, key) => (
-        <ComponentDocumentation
-          key={key}
-          {...props}
-          className="border-grey mb-8 border-b-2 pb-8 last:mb-0"
-        />
-      ))}
-
-      {/* <ComponentDocumentation
-        name="Header 2"
-        code={`<h1>h2</h1>`}
-        render={<h2>h2</h2>}
-      />
-
-      <h2>h2</h2>
-      <h3>h3</h3>
-      <h4>h4</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        alias nam fuga iste cumque! Exercitationem molestiae quo porro eos
-        incidunt qui natus earum laudantium, veritatis, perspiciatis unde
-        eveniet rem atque!
-      </p>
-      <p className="small">Lorem ipsum dolor</p>
-      <blockquote>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        alias nam fuga iste cumque! Exercitationem molestiae quo porro eos
-        incidunt qui natus earum laudantium, veritatis, perspiciatis unde
-        eveniet rem atque!
-      </blockquote>
-
-      <ul>
-        <li>list item 1</li>
-        <li>list item 2</li>
-        <li>list item 3</li>
-      </ul>
-
-      <ol>
-        <li>list item 1</li>
-        <li>list item 2</li>
-        <li>list item 3</li>
-      </ol>
-
-      <h1>Buttons</h1> */}
+      <h1 className="mb-8">Hello World</h1>
     </main>
   );
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -34,7 +42,22 @@ const config: Config = {
         },
       },
     },
+
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
 };
 export default config;
