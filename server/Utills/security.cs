ddd5c0a6d1389c3afb5ae8ed5a000fac7b3b2900
Mimi <makolamichele@gmail.com>
2023-09-18 15:17:19 +0200

    added fixed code for security

--- server/Utills/security.cs
+++ server/Utills/security.cs
@@ -7,9 +7,9 @@ namespace Utility{
         public static string hashauth(string input){
         string hashauth =string.Empty;
 
-        using(SHA256 sha = SHA256.CreateBuilder()){
+        using(SHA256 sha = SHA256.Create()){
 
-            byte[] value =sha.ComputeHash(Enconding.UTF8.GetBytes[input]);
+            byte[] value =sha.ComputeHash(Enconding.UTF8.GetBytes(input));
 
             foreach(byte b in value){
                 hashauth +=b;
