7c92b00a1c5be17143d35b3e746fd1f02ddb14fd
Trevor-Dam <trevordamoyi307@gmail.com>
2023-09-28 12:14:57 +0200

    Imperfect JWT Setup

--- server/Program.cs
+++ server/Program.cs
@@ -5,12 +5,46 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Models;
 using Routes;
 using GalleriaMiddleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Cryptography;
+using File = System.IO.File;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.JsonWebTokens;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
+var keys = RSA.Create();
+keys.ImportRSAPrivateKey(File.ReadAllBytes("keys"), out _);
 // builder.Services.AddDbContext<GalleriaHubDBContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Lite")));
 builder.Services.AddSqlite<GalleriaHubDBContext>(builder.Configuration.GetConnectionString("Lite"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("jwt")
+    .AddJwtBearer("jwt", gh => {
+        gh.TokenValidationParameters = new TokenValidationParameters() {
+            ValidateAudience = false,
+            ValidateIssuer = false
+        };
+        
+        gh.Events = new JwtBearerEvents() {
+            OnMessageReceived = (querh) => {
+                if (querh.Request.Query.ContainsKey("user")) {
+                    querh.Token = querh.Request.Query["user"];
+                }
+                return Task.CompletedTask;
+            }
+        };
+
+        gh.Configuration = new OpenIdConnectConfiguration() {
+            SigningKeys = {
+                new RsaSecurityKey(keys)
+            }
+        };
+    })
+;
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 app.UseSwagger();
@@ -18,11 +52,24 @@ app.UseSwaggerUI();
 
 app.UseDatabaseConnectionTest();
 
-app.MapGet("/", ()=> "Hello World");
+app.UseAuthentication();
+
+app.MapGet("/", (HttpContext htcx) => htcx.User.FindFirst("GalleriaHub")?.Value ?? "empty");
 
 app.MapGet("/db-connected", (HttpContext context) => {
     var DB = context.RequestServices.GetRequiredService<GalleriaHubDBContext>();
     return Results.Ok(DB != null);
+});
+
+app.MapGet("/jwt", (string input) => {
+    var handler = new JsonWebTokenHandler();
+        var token = handler.CreateToken(new SecurityTokenDescriptor() {
+            Issuer = "https://localhost:5000",
+            Subject = new ClaimsIdentity(new [] {
+                new Claim("Email", input.Email)
+            })
+        });
+        return token;
 });
 
 app.MapGroup(Developers.RouterPrefix)
@@ -30,8 +77,7 @@ app.MapGroup(Developers.RouterPrefix)
 
 app.MapGroup(Routes.User.RouterPrefix)
     .UserEndpoints();
-    // .RequireAuthorization();
-
+    //require Authorization
 app.Run();
 
 public class TeamMember {
--- server/README.md
+++ server/README.md
@@ -1,7 +1,8 @@
 # GalleriaHub Backend
 
-# Table of Content
+## Table of Content
 
 ## Service Documentation
 
-The tool we decided to use to document our web service will be Swagger, more information can be found on <https://swagger.io>. The documentation is in a JSON format and can be accessed by starting the service in this folder and typing [host name]/[port number]/swagger/v1/swagger.json.
+The tool we decided to use to document our web service will be Swagger, more information can be found on <https://swagger.io>. The documentation is in a JSON format and can be accessed by starting the service in this folder and typing
+<[host name]/[port number]/swagger/v1/swagger.json>.
