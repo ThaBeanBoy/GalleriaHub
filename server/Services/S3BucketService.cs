d88395823ed875559fa4a1cef5aef23ea127f1f8
ThaBeanBoy <gtchipoyera@gmail.com>
2023-10-08 18:02:52 +0200

    Made an empty S3 bucket service

---
+++ server/Services/S3BucketService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Services;
+
+public class S3BucketService
+{
+    private readonly IConfigurationSection JwtSettings;
+
+    public S3BucketService(IConfigurationSection JwtSettings)
+    {
+        this.JwtSettings = JwtSettings;
+    }
+
+    // upload
+
+    // download
+
+    // delete
+}
+
+public static class S3BucketServiceExtension
+{
+    public static IServiceCollection AddS3Bucket(this IServiceCollection serviceCollection, IConfigurationSection JwtSettings)
+    {
+        return serviceCollection.AddSingleton(_ => new S3BucketService(JwtSettings));
+    }
+}
