c667cf49f4dff8703b110f9c038c08599b3b4e81
Tr4m <morenaramza@gmail.com>
2023-10-11 13:27:55 +0200

    added functionality to delete item from the list

--- server/Routes/List.cs
+++ server/Routes/List.cs
@@ -78,6 +78,72 @@ public static class List
             }
         });
 
+        // Delete a product from a user's wishlist
+        group.MapDelete("/{ListID}/{productId}", (HttpContext context) => {
+            var (Request, Response) = (context.Request, context.Response);
+
+            try {
+                Models.User? User = context.Items["User"] as Models.User;
+
+                if (User == null) {
+                    Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Response.WriteAsync("Only logged-in users can remove products from their wishlist");
+                }
+
+                var DB = context.RequestServices.GetRequiredService<GalleriaHubDBContext>();
+
+                // Parse the product ID and wishlist ID from the route
+                int productId = int.Parse(context.GetRouteValue("productId") as string ?? "0");
+                int ListId = int.Parse(context.GetRouteValue("ListID") as string ?? "0");
+
+                // Find the product with the specified ID
+                Models.Product? product = DB.Products.FirstOrDefault(p => p.ProductID == productId);
+
+                if (product == null) {
+                    Response.StatusCode = StatusCodes.Status404NotFound;
+                    return Response.WriteAsync("Product not found");
+                }
+
+                // Find the user's wishlist based on ListID
+                Models.List? wishlist = DB.Lists.FirstOrDefault(w => w.ListID == ListId);
+
+                if (wishlist == null) {
+                    Response.StatusCode = StatusCodes.Status404NotFound;
+                    return Response.WriteAsync("Wishlist not found");
+                }
+
+                if (wishlist.UserID != User.UserID) {
+                    Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Response.WriteAsync("You don't own the wishlist");
+                }
+
+                // Check if the product is in the user's wishlist
+                Models.ListItem wishlistItem = DB.ListItems.FirstOrDefault(wi => wi.ProductID == product.ProductID && wi.ListID == ListId);
+
+                if (wishlistItem != null) {
+                    // Remove the product from the user's wishlist
+                    DB.ListItems.Remove(wishlistItem);
+                    DB.SaveChanges();
+
+                    Response.StatusCode = StatusCodes.Status204NoContent;
+                    return Response.WriteAsync("Product removed from wishlist");
+                } else {
+                    Response.StatusCode = StatusCodes.Status404NotFound;
+                    return Response.WriteAsync("Product not found in the wishlist");
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                return Response.WriteAsync("Something went wrong with the server");
+            }
+        });
+
+
+        //retrieve list
+
+        //make lists(create)
+
+        //Delete list
         return group;
     }
 }
